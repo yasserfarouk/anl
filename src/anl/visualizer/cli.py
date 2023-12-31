57cb6c383f7e0644115cf86b142b8954f640e61f
Yasser Mohammad <yasserfarouk@gmail.com>
2023-12-31 17:34:28 +0900

    Adding --port, --address to anlv show
    You can now set the port and address of the visualizer

--- src/anl/visualizer/cli.py
+++ src/anl/visualizer/cli.py
@@ -24,7 +24,21 @@ def main():
     default=None,
     help=f"Folder containing logs of a tournament to open. If not given, All runs at {DEFAULT_TOURNAMENT_PATH} will be used",
 )
-def show(folder: Path):
+@click.option(
+    "-p",
+    "--port",
+    type=int,
+    default=8501,
+    help=f"The port to run the visualizer on",
+)
+@click.option(
+    "-a",
+    "--address",
+    type=str,
+    default="0.0.0.0",
+    help=f"The address to run the visualizer on",
+)
+def show(folder: Path, port: int, address: str):
     # folder = Path(folder) if folder is not None else None
     if folder:
         sys.argv = [
@@ -35,6 +49,10 @@ def show(folder: Path):
         ]
     else:
         sys.argv = ["streamlit", "run", str(Path(__file__).parent / "app.py")]
+    if port:
+        sys.argv += ["server.port", str(port)]
+    if address not in ("", "default"):
+        sys.argv += ["server.address", address]
     sys.exit(stcli.main())
 
 
